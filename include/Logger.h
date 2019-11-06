8b1af02a483f23e30768f67ae31b85b2eebf3886
JSteilberg <jsteil123@gmail.com>
2019-11-06 09:45:36 -0600

    Move logger functionality to header; make logger generic

--- include/Logger.h
+++ include/Logger.h
@@ -5,6 +5,7 @@
 #include <boost/lexical_cast.hpp>
 #include <ctime>
 #include <iostream>
+#include <sstream>
 
 #include "Defines.h"
 
@@ -33,18 +34,65 @@ class Logger {
 public:
   // Logs (or doesn't) the given information with the given severity level,
   // along with a timestamp
-  static void Log(const std::string &msg, SeverityLevel level);
+  template <class T> static void Log(const T &msg, SeverityLevel level) {
+    std::stringstream ss;
+    ss << msg;
+    string msg_str = ss.str();
 
-  // Logs (or doesn't) the given information with the given severity level,
-  // along with a timestamp
-  static void Log(int msgNum, SeverityLevel level);
+    string logmsg = boost::algorithm::replace_all_copy(
+        msg_str, "\n", "\n                        -> ");
+    if (level == HIGH) {
+      std::cerr << TimeStr() << " [High] " << logmsg << std::endl;
+    } else if (level == MED) {
+      std::cerr << TimeStr() << " [Med ] " << logmsg << std::endl;
+    } else if (level == LOW) {
+#ifdef DEBUG_MODE
+      std::cerr << TimeStr() << " [Low ] " << logmsg << std::endl;
+#endif
+    } else if (level == INFO) {
+#ifdef DEBUG_MODE
+      std::cout << TimeStr() << " [Info] " << logmsg << std::endl;
+#endif
+    }
+  }
 
   // Logs (or doesn't) the given information without any severity level or
   // timestamp
-  static void RawLog(const std::string &msg, SeverityLevel level);
+  template <typename T> static void RawLog(const T &msg, SeverityLevel level) {
+    std::stringstream ss;
+    ss << msg;
+    string msg_str = ss.str();
 
+    string logmsg = "                        -> " +
+                    boost::algorithm::replace_all_copy(
+                        msg_str, "\n", "\n                        -> ");
+    if (level == HIGH) {
+      std::cerr << logmsg << std::endl;
+    } else if (level == MED) {
+      std::cerr << logmsg << std::endl;
+    } else if (level == LOW) {
+#ifdef DEBUG_MODE
+      std::cerr << logmsg << std::endl;
+#endif
+    } else if (level == INFO) {
+#ifdef DEBUG_MODE
+      std::cout << logmsg << std::endl;
+#endif
+    }
+  }
   // Returns the current time in m-d-y h:m:s format
-  static string TimeStr();
+  static string TimeStr() {
+    time_t rawtime;
+    struct tm *timeinfo;
+    char buffer[80];
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer, sizeof(buffer), "%m-%d-%Y %I:%M:%S", timeinfo);
+
+    return string(buffer);
+  }
 
 protected:
   // dont you dare
