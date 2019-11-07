a9b926ba1309755f010d5a31533e359a45ebb334
JSteilberg <jsteil123@gmail.com>
2019-11-07 00:14:12 -0600

    Forgot this file, goes to above commit

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,20 +2,21 @@
 
 Game::Game()
     : game_config_("./res/cfg/config.json"),
-      window_(sf::VideoMode(game_config_.GetIntVar("window_width"),
-                            game_config_.GetIntVar("window_height")),
+      window_width_(game_config_.GetIntVar("window_width")),
+      window_height_(game_config_.GetIntVar("window_height")),
+      window_(sf::VideoMode(window_width_, window_height_),
               "Avarus v" + consts::kGameVersion),
       id_registry_("./res/cfg/ids.json"),
       game_atlas_("./res/cfg/atlas.json", id_registry_), gravity_(0.0f, 0.0f),
-      world_(gravity_), player_(game_atlas_, world_),
-      dbg_overlay_(), 
-      console_overlay_() {
-    
-  main_loop_ = std::make_shared<MainLoop>(player_, world_, dbg_overlay_, console_overlay_);
+      world_(gravity_), player_(game_atlas_, world_), dbg_overlay_(),
+      console_overlay_(window_width_, window_height_, 500, 300) {
+
+  main_loop_ = std::make_shared<MainLoop>(shared_ptr<Game>(this));
+  //        player_, world_, dbg_overlay_, console_overlay_);
 
   // window_.setFramerateLimit(60);
   window_.setKeyRepeatEnabled(false);
-  state_stack_.push(main_loop_);
+  state_list_.push_front(main_loop_);
 
   dbg_overlay_.Set("ver", string("Avarus v") + consts::kGameVersion);
   dbg_overlay_.Set("thingsps", "0");
@@ -35,15 +36,17 @@ int Game::Start() {
   // Main loop
   while (window_.isOpen()) {
     window_.clear();
-
     if (update_clock_.getElapsedTime().asSeconds() >=
         1.0 / consts::kUpsPerSec) {
       // world_.Step(update_clock_.getElapsedTime().asSeconds(), 6, 2);
       update_clock_.restart();
-      state_stack_.top()->Update(delta_clock_.restart(), window_);
+      state_list_.front()->Update(delta_clock_.restart(), window_);
       updates = updates + 1;
     }
-    state_stack_.top()->Draw(window_);
+
+    for (auto &state : state_list_) {
+      state->Draw(window_);
+    }
     window_.display();
 
     frames = frames + 1;
@@ -57,6 +60,10 @@ int Game::Start() {
     }
   }
 
+  for (auto &new_state : new_states_) {
+    state_list_.push_front(new_state);
+  }
+
   /*while (!state_stack_.empty()) {
       GameState* gs = state_stack_.top();
       Logger::Log("Delete state: " + gs->ToString(), INFO);
@@ -68,6 +75,10 @@ int Game::Start() {
 
 // Maybe remove this?
 void Game::Update(const sf::Event &event) {}
+
+void Game::AddState(shared_ptr<GameState> new_state) {
+  new_states_.push_front(new_state);
+}
 
 Game::~Game() {
   // dtor
